5f870ea06ae53aa1d3cffaabf1f7ef9b9f5ec76d
Soumeh <stefi.brezovac@gmail.com>
2022-07-27 14:20:52 +0200

    update bot starting and some dev mode work

--- principality/__init__.py
+++ principality/__init__.py
@@ -11,93 +11,142 @@ from principality.overseer import Overseer
 
 class Principality(Bot, Configurable):
 
-    config_file = 'configs/Bot.toml'
+    config_directory = Path('configs/')
+    config_file = config_directory/'Bot.toml'
 
     class Config:
+        cog_directory: str = Option('cogs', "What directory to load modules from")
+
         token_env_var: str = Option('TOKEN', "The environmental variable of your bot's token. (Leave as default unless you want to work with multiple bots)")
-        cog_folder: str = Option('cogs', "What directory to load modules from")
         cog_databases: bool = Option(True, "Whether or not every cog should generate a database for itself (accessible using `self.db`)")
         default_database_type: Literal['local', 'temp', 'deta'] = Option('local', "What database type to use for cogs (Options: local, temp or deta)")
 
     def __init__(self, *args):
+
         Bot.__init__(self, rollout_all_guilds=True, intents=Intents.all(), allowed_mentions=AllowedMentions(everyone=False, replied_user=False), *args)
         Configurable.__init__(self)
 
     def load(self):
-        cogs = get_cogs(Path(self.config.cog_folder))
+        print('Loaded Cogs:')
+        cogs = get_cogs(Path(self.config.cog_directory))
         for cog in cogs.values():
             db = None
             if self.config.cog_databases: db = self.config.default_database_type
             cog = cog(db)
+            self.add_cog(cog)
             self.load_cog(cog)
-        print('Loaded Cogs: \n' + '\n'.join(self.cogs.keys()))
+            print('> '+cog.name)
 
     async def on_ready(self):
-        for name, cog in self.cogs.items():
-            try:
-                await self.async_load_cog(cog)
-            except Exception as error:
-                exception_type = type(error).__name__
-                exception_message = error
-                exception_line = error.__traceback__.tb_lineno
-                print(f"Error in cog '{name}' on line {exception_line}: [{exception_type}] {exception_message}")
+        for cog in self.cogs.values():
+            await self.async_load_cog(cog)
+
+    def add_cog(self, cog: Cog):
+        cog.bot = self
+        super().add_cog(cog, override=True)
+        return True
 
     def load_cog(self, cog: Cog):
-        cog.bot = self
-        self.add_cog(cog, override=True)
-        if hasattr(cog, 'load'): cog.load()
+        try:
+            if hasattr(cog, 'load'): cog.load()
+            return True
+        except Exception as error:
+            exception_type = type(error).__name__
+            exception_message = error
+            exception_line = error.__traceback__.tb_lineno
+            print(f"Error while loading cog '{cog.name}' on line {exception_line}: [{exception_type}] {exception_message}")
 
     async def async_load_cog(self, cog: Cog):
-        if hasattr(cog, 'ready'): await cog.ready()
+        try:
+            if hasattr(cog, 'ready'): await cog.ready()
+            return True
+        except Exception as error:
+            exception_type = type(error).__name__
+            exception_message = error
+            exception_line = error.__traceback__.tb_lineno
+            print(f"Error while readying cog '{cog.name}' on line {exception_line}: [{exception_type}] {exception_message}")
 
-    async def unload_cog(self, cog: Cog):
-        self.remove_cog(cog.__cog_name__)
+    def unload_cog(self, cog_name: str):
+        self.remove_cog(cog_name)
+        return True
 
 
 
 class PrincipalityDev(Principality):
 
+    found_cogs = {}
+
     async def on_ready(self):
         await super().on_ready()
-        self.config_overseer = Overseer(self.config_folder, '*.toml')
-        self.cog_overseer = Overseer(Path(self.config['cog_folder']), ['**/*.py', '**/*.json'])
+        self.config_overseer = Overseer(self.config_directory, '*.toml')
+        self.cog_overseer = Overseer(Path(self.config.cog_directory), ['**/*.py', '**/*.json'])
         await self.update_checker.start()
-
-
 
     @tasks.loop(seconds=5)
     async def update_checker(self):
 
         # check configs
-        changed, added, removed = self.config_overseer.changes()
-
+        changed, _, removed = self.config_overseer.changes()
         for file in changed.union(removed):
-            file_name = file.stem
-            cog_folder = Path(self.config['cog_folder']) / file_name
-            module = import_module(Path(self.config['cog_folder']).stem+'.'+file_name)
-            for temp_cog in self.cogs_from_module(module):
-                cog_name = temp_cog().__cog_name__
-                cog = self.cogs[cog_name]
-                cog.reload_config()
-                print(f"Reloaded config for cog '{cog_name}'")
+            cog_id = file.stem
+            cog = self.cogs.get(cog_id, None)
+            if not cog:
+                print("Non-Cog configuration file edited, you'll have to restart your bot manually to apply changes")
+                continue
+            cog.reload_config()
+            print(f"Reloaded config for cog '{cog_id}'")
 
         # check cogs
+        self.initialized_cogs = []
         changed, added, removed = self.cog_overseer.changes()
-        functions = {'changed': changed, 'added': added, 'removed': removed}
-
+        functions = {'added': added, 'changed': changed, 'removed': removed} # layout determines priority
         for function, list in functions.items():
             for file in list:
-                file_name = file.parent.stem
-                cog_folder = Path(self.config['cog_folder']) / file_name
+                # find cog id and cache it
+                cog_dir = await self.find_cog(file)
+                if not cog_dir:
+                    print(f"Couldn't find what cog file '{file}' belongs to")
+                    continue
+                if cog_dir in self.initialized_cogs:
+                    continue
 
-                module = import_module(Path(self.config['cog_folder']).stem+'.'+file_name)
-                if function == 'changed':
-                    reload(module)
+                cog = Cog.from_dir(cog_dir)
+                self.found_cogs[path] = cog
 
-                for cog in self.cogs_from_module(module):
-                    cog = cog()
-                    if function != 'removed':
-                        await self.load_cog(cog, cog_folder)
-                        await self.async_load_cog(cog)
-                    else:
-                        await self.unload_cog(cog)
+                if function == 'removed':
+                    self.unload_cog(cog_dir.stem)
+                    continuev
+
+                cog = cog()
+
+                if function == 'added':
+                    cog.bot = self
+                    self.add_cog(cog)
+
+                cog = cog()
+                self.load_cog(cog)
+                await self.async_load_cog(cog)
+
+                self.initialized_cogs.append(cog_dir)
+
+    async def find_cog(self, path: Path) -> str:
+        cog_dir = self.found_cogs.get(path, None)
+        if cog_dir and cog_dir not in self.initialized_cogs:
+            return None
+        if not cog_dir:
+            cog_dir = self._find_cog(path)
+            if not cog_id:
+                return None
+
+        self.found_cogs[path] = cog_dir
+        return cog_dir
+
+    def _find_cog(self, path: Path) -> str:
+        if path == Path('.'):
+            return None
+        files = [i for i in path.glob('pyproject.toml')]
+        if len(files) < 1:
+            return self._find_cog(path.parent)
+        if len(files) > 1:
+            return None
+        return files[0].parent.stem
--- principality/__main__.py
+++ principality/__main__.py
@@ -1,30 +1,68 @@
-from typer import Typer
+from typer import Typer, Option
 from os import getenv
 from dotenv import load_dotenv
+
+from nextcord.errors import PrivilegedIntentsRequired, LoginFailure
 
 app = Typer(pretty_exceptions_show_locals=False, pretty_exceptions_short=True)
 load_dotenv()
 
+@app.callback()
+def main():
+    pass
+
 @app.command()
-def start(dev_mode: bool = False):
+def start(dev_mode: bool = Option(False, "--dev", help="Start the bot in Development Mode")):
+    """Start your Principality Bot"""
 
     if dev_mode:
         from principality import PrincipalityDev
         client = PrincipalityDev()
+        print('Starting Principality in Development Mode...\n')
     else:
         from principality import Principality
         client = Principality()
+        print('Starting Principality...\n')
 
     token = getenv(client.config.token_env_var)
     if not token:
-        print("A Discord bot token must be provided in an environmental variable in order to start.")
-    else:
+        print("""To get started, you have to provide a Discord Bot token
+If you have not made a Discord Bot or do not know how to get its token, follow this guide:
+https://discordpy.readthedocs.io/en/stable/discord.html
+
+This token is stored in the .env file, for future use
+In case you enter an invalid token, you can change it inside of the .env file by replacing the text after TOKEN=\n""")
+        token = input("Token: ")
+        if not token: return print("A Discord Bot token must be provided in order to start.")
+
+        with open('.env', 'r') as file:
+            env = file.read().split('\n')
+        for val in env:
+            if val.startswith('TOKEN='): env[env.index(val)] = 'TOKEN=' + token
+        with open('.env', 'w') as file:
+            file.write('\n'.join(env))
+
+    try:
         client.load()
         client.run(token)
+    except PrivilegedIntentsRequired as error:
+        url = f'https://discord.com/developers/applications/{client.id}/bot'
+        print(error)
+    except LoginFailure as error:
+        print('You have provided an improper Discord Bot token, you can replace it with a proper one inside of the .env file')
 
 @app.command()
 def info():
-    pass
+    """W.I.P."""
+
+@app.command()
+def setup_heroku():
+    """Set up all of the files necessary to host on Heroku"""
+    with open('Procfile', 'w+') as file:
+        file.write('worker: python -m principality start')
+    with open('runtime.txt', 'w+') as file:
+        file.write('python-3.10.5')
+    print("Created Heroku files")
 
 if __name__ == '__main__':
     app()
